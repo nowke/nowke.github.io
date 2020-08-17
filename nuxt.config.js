51eac7d90702dcd2228815c1220b0823b780bd2a
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2020-08-17 01:18:34 -0700

    Add GA tracking

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['@/plugins/vue-scrollactive'],
+  plugins: ['@/plugins/vue-scrollactive', '@/plugins/ga'],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
